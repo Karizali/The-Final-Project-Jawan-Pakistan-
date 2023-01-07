9bf7948bc01fb338a214de19ac252e4a97b5cb8d
Karizali <96954751+Karizali@users.noreply.github.com>
2023-01-07 02:39:01 -0800

    first commit

---
+++ Project/app.js
@@ -0,0 +1,3 @@
+function locationmap(){
+    window.location.assign('https://www.google.com/maps/place/King+Broast+and+Pizza/@24.8283867,67.1571659,16.75z/data=!4m5!3m4!1s0x3eb33a58d2cadd23:0x5f404ccc9dc4ebc4!8m2!3d24.8280625!4d67.1596875')
+}
